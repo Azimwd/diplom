cb8b1dfe6a478eb9ec6c42d2e5d50f9a7e040329
Azimwd <programminsince2023@gmail.com>
2026-06-04 01:08:30 +0500

    fix 3 tg language

--- Lawly/settings.py
+++ Lawly/settings.py
@@ -196,26 +196,16 @@ GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", "")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", "")
 
 SOCIALACCOUNT_PROVIDERS = {
-    "google": {
-        "APPS": [
-            {
-                "client_id": GOOGLE_CLIENT_ID,
-                "secret": GOOGLE_CLIENT_SECRET,
-                "key": "",
-            }
-        ],
-        "SCOPE": [
-            "profile",
-            "email",
-        ],
-        "AUTH_PARAMS": {
-            "access_type": "online",
-            "prompt": "select_account",
-        },
-        "OAUTH_PKCE_ENABLED": True,
+    'google': {
+        'SCOPE': ['profile', 'email'],
+        'AUTH_PARAMS': {'access_type': 'offline'},
+        'APP': {
+            'client_id': GOOGLE_CLIENT_ID,
+            'secret': GOOGLE_CLIENT_SECRET,
+            'key': ''
+        }
     }
 }
-
 
 # =========================
 # GOOGLE OAUTH
